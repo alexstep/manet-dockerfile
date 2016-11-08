297b81dcc6e7cabd05455e9f5aac9f671dc12a61
AlexStep <alex.step.kellas@gmail.com>
2016-11-08 14:38:16 +0200

    clean

--- entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/usr/bin/manet --command="/usr/bin/xvfb-daemon-run /usr/bin/slimerjs" --level=debug
--- run-dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker stop manet
-docker rm manet
-docker build --tag="manet" .
-docker run --name manet -p 8891:8891 --add-host=dev.localhost:192.168.59.1 -d manet
